4a1de2a575a80527d26d9083beb4c85b189833b2
Rui Baiao <rui@frostdigital.se>
2016-12-01 14:49:38 +0100

    new modal external lib added

--- app/components/gallery-modal/directive-gallery-modal.js
+++ app/components/gallery-modal/directive-gallery-modal.js
@@ -20,8 +20,22 @@ PortfolioDirectives.directive('galleryModal', function(){
 			};
 
 			scope.switchImage = function(direction){
-				debugger;	
-			}
+				
+				var imageToSet = null;
+				_.each(scope.gallery, function(photo, idx){
+					//if the main photo is found, change it, depending on the direction									
+					if(photo.image.id === scope.mainImage.id && imageToSet === null){
+						if(direction === 'next'){
+							imageToSet = scope.gallery[idx+1].image;							
+						}
+						else if(direction === 'previous'){
+							imageToSet = scope.gallery[idx-1].image;
+						}
+					}
+				});
+				scope.mainImage = imageToSet;
+
+			};
 
 			init();
 
